50178be417cb7a7a5ff42bd857b31ff611d1020b
nadirhussain786 <nh262464@gmail.com>
2024-09-04 09:21:43 -0700

    testing pull request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import supabase from "@/lib/supabase/client";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       Shoes Rack APP
+       Shoes Rack APP Work
     </main>
   );
 }
