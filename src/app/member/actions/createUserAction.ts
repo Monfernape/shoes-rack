366b1dc2d9f39b6f849101a41cf581371af79104
Mubashar Shakeel <moonmubashar945@gmail.com>
2024-11-01 18:32:51 +0500

    Changed Some Fixes

--- src/app/member/actions/createUserAction.ts
+++ src/app/member/actions/createUserAction.ts
@@ -5,13 +5,16 @@ import { Tables } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { Routes } from "@/lib/routes";
 import { getSupabaseClient } from "../../../../utils/supabase/supabaseClient";
+import { UserStatus } from "@/constant/constant";
 
 export const createUser = async (values: z.infer<typeof userBuilderSchema>) => {
   const supabase = await getSupabaseClient();
 
   const { error } = await supabase.from(Tables.Members).insert({
     ...values,
-    status: "inactive",
+    date_of_birth: values.date_of_birth.toISOString(),
+    ehad_start_date: values.ehad_start_date.toISOString(),
+    status: UserStatus.Inactive,
   });
 
   if (error) {
