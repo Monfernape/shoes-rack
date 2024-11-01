6211ef1d495396d50a553e2ad5956e1c0cfab17d
nadirhussain786 <nh262464@gmail.com>
2024-11-01 17:39:02 +0500

    work on PR comments

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { MemberList } from "./components/MemberList";
 import { getMembers } from "@/app/member/actions/getMembers";
 
