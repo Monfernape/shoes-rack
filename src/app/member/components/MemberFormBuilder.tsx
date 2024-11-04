daf583f0247bf6e7ef9856b7b6298903932f2749
Mubashar Shakeel <moonmubashar945@gmail.com>
2024-11-04 12:52:29 +0500

    Fixed latest Commments

--- src/app/member/components/MemberFormBuilder.tsx
+++ src/app/member/components/MemberFormBuilder.tsx
@@ -14,6 +14,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { addDays, format } from "date-fns";
+import { useRouter } from "next/navigation";
 import { Shift, UserRole } from "@/constant/constant";
 import {
   Select,
@@ -40,6 +41,7 @@ import {
 } from "../../../../regex";
 import { createUser } from "../actions/action";
 import { useToast } from "@/hooks/use-toast";
+import { Routes } from "@/lib/routes";
 
 export type UserBuilder = z.infer<typeof userBuilderSchema>;
 
@@ -105,6 +107,7 @@ export const userBuilderSchema = z.object({
 
 export const MemberFormBuilder = () => {
   const { toast } = useToast();
+  const router = useRouter();
   const phoneNumberMask = useMask({
     mask: "03__-_______",
     replacement: { _: /\d/ },
