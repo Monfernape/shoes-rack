8e39bab3979ec5fffdfc102e8c011489bbfe644d
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2024-11-05 15:45:58 +0500

    Configure Github action for prettier

---
+++ .github/workflows/Testing.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Testing = () =>   {
+    return(
+                                        <div>
+            Testing Component
+                       <h1>Hello World!</h1>
+           <p>Hello</p>          
+        </div>
+    )
+}
