b42695378573c507ba3e7aabbdd287c771f738d5
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2024-11-05 19:08:01 +0500

    fix: spacing

--- .github/workflows/Testing.tsx
+++ .github/workflows/Testing.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 const Testing = () =>   {
     return(
                                         <div>
-            Testing Component
-                       <h1>Hello World!</h1>
-           <p>Hello</p>          
+            Testing Component       
+                                <h1>Hello World!</h1>
+            <h1>Hello World!</h1>
+                                <p>Hello</p>          
         </div>
     )
 }
