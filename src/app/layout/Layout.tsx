693c3c0f29ab41cf385ef18014d62148976c8f36
muhammad-bilal-ali-khokhar <o.engr.bilal@gmail.com>
2024-11-13 18:17:16 +0500

    fix: form UI fixes

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 import { Sidebar } from "./components/Sidebar";
+import { cx } from "class-variance-authority";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -17,7 +18,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
       {(pathname !== "/login" || isSidebarOpen) && (
         <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       )}
-      <div className="flex flex-col w-full h-screen lg:ml-48">
+      <div
+        className={cx(
+          `flex flex-col w-full h-screen ${pathname !== "/login" && "lg:ml-48"}`
+        )}
+      >
         {children}
       </div>
     </div>
