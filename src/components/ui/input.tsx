6cee33d0aaae2a1caca67ddc514c416cfde9b313
Mubashar Shakeel <moonmubashar945@gmail.com>
2024-11-13 13:57:35 +0500

    Fix Lint Issues

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  hasError ?: Boolean;
+  hasError?: boolean;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
