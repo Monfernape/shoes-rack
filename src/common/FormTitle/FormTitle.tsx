5bcfc8e4082b435237c9ea5c9e811d71e0353fa4
nadirhussain786 <nh262464@gmail.com>
2024-11-13 12:41:25 +0500

    work on form title comments

--- src/common/FormTitle/FormTitle.tsx
+++ src/common/FormTitle/FormTitle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const FormTitle = ({ title }: { title: string }) => {
   return (
-    <h1 className="text-sm text-sm font-semibold text-gray-800 my-4">
+    <h1 className="text-sm font-semibold text-gray-800 my-4">
       {title}
     </h1>
   );
