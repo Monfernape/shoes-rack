9d6bd019ee1ac4eb4b82ef7287a46f94fa99928c
nadirhussain786 <nh262464@gmail.com>
2024-11-13 19:43:07 +0500

    work on useMamo implemention on array

--- src/app/leaves/components/LeaveActionRender.tsx
+++ src/app/leaves/components/LeaveActionRender.tsx
@@ -39,44 +39,53 @@ const LeaveTableActionRender = ({ leaveRequestDetails }: Props) => {
     console.log("Rejected Request...");
   };
 
-  const baseActions = [
-    {
-      title: "Edit",
-      id: 2,
-      onClick: handleEditInfo,
-      icon: <EditIcon size={16} />,
-    },
-    {
-      title: "Delete",
-      id: 3,
-      onClick: handleDeleteRequest,
-      icon: <TrashIcon size={16} className="stroke-status-inactive" />,
-    },
-  ];
+  const baseActions = useMemo(
+    () => [
+      {
+        title: "Edit",
+        id: 2,
+        onClick: handleEditInfo,
+        icon: <EditIcon size={16} />,
+      },
+      {
+        title: "Delete",
+        id: 3,
+        onClick: handleDeleteRequest,
+        icon: <TrashIcon size={16} className="stroke-status-inactive" />,
+      },
+    ],
+    []
+  );
 
-  const viewInfo = [
-    {
-      title: "View Details",
-      id: 1,
-      onClick: handleViewDetails,
-      icon: <InfoIcon size={16} />,
-    },
-  ];
+  const viewInfo = useMemo(
+    () => [
+      {
+        title: "View Details",
+        id: 1,
+        onClick: handleViewDetails,
+        icon: <InfoIcon size={16} />,
+      },
+    ],
+    []
+  );
 
-  const statusActions = [
-    {
-      title: "Approve",
-      id: 4,
-      onClick: handleApprovedRequest,
-      icon: <CheckCircleIcon size={16} />,
-    },
-    {
-      title: "Reject",
-      id: 4,
-      onClick: handleRejectedRequest,
-      icon: <AlertCircleIcon size={16} className="stroke-status-inactive" />,
-    },
-  ];
+  const statusActions = useMemo(
+    () => [
+      {
+        title: "Approve",
+        id: 4,
+        onClick: handleApprovedRequest,
+        icon: <CheckCircleIcon size={16} />,
+      },
+      {
+        title: "Reject",
+        id: 4,
+        onClick: handleRejectedRequest,
+        icon: <AlertCircleIcon size={16} className="stroke-status-inactive" />,
+      },
+    ],
+    []
+  );
 
   const actionMenu = useMemo(() => {
     switch (loggedUser.role) {
