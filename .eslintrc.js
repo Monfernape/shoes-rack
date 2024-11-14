91ed2e5a7e09324e07f715fa51856a504bbc95f0
Mubashar Shakeel <moonmubashar945@gmail.com>
2024-11-14 12:33:43 +0500

    Resolved Comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
     "next",
     "next/core-web-vitals",
     "eslint:recommended",
-    // "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
   ],
   parserOptions: {
