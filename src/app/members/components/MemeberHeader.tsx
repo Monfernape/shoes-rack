5b2e6851eb901a676d6b824a1267985187f85453
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2024-11-15 21:02:45 +0500

    Fixed

--- src/app/members/components/MemeberHeader.tsx
+++ src/app/members/components/MemeberHeader.tsx
@@ -6,24 +6,19 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { HamburgerMenuIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { usePathname } from "next/navigation";
-import { useRouter } from "next/router";
 import { MemberBreadCrumbs } from "./MemberBreadCrumbs";
 import { Plus } from "lucide-react";
 import { Routes } from "@/lib/routes";
 
-
 export const MemeberHeader = () => {
   const pathname = usePathname();
-  const router = useRouter();
+
   const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
   const [isTitleHide, setIsTitleHide] = useState<boolean>(false);
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
- const  navigateToAddMember=()=>{
-    router.push(Routes.AddMember)
- }
 
   return (
     <div className="sticky top-0 z-50 w-full">
@@ -64,7 +59,7 @@ export const MemeberHeader = () => {
                 />
                 <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600 w-4 h-4" />
               </div>
-              <Button data-testid="addMemberButton" className="h-7"  onClick={navigateToAddMember}>
+              <Button data-testid="addMemberButton" className="h-7">
                 <Plus /> <span className="hidden md:block">Add Member </span>
               </Button>
             </div>
