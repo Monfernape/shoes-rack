623166c4e796a88319571f2e3e9d483a48d54999
muhammad-bilal-ali-khokhar <o.engr.bilal@gmail.com>
2024-12-02 13:56:43 +0500

    fixed: build error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Suspense>children</Suspense>
+        <Suspense>{children}</Suspense>
       </body>
     </html>
   );
