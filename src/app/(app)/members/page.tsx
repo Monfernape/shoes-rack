59da1780919218066d2a2166db0b17ff633468d2
Mubashar Shakeel <moonmubashar945@gmail.com>
2024-12-04 18:18:12 +0500

    Fixed Lint issue

--- src/app/(app)/members/page.tsx
+++ src/app/(app)/members/page.tsx
@@ -5,7 +5,6 @@ import { PageLayout } from "@/app/layout/PageLayout";
 import { getMembers } from "./actions/getMembers";
 import { Breadcrumbs } from "@/types";
 import { Routes } from "@/lib/routes";
-import { getLoggedInUser } from "@/utils/getLoggedInUser";
 
 const breadcrumbs: Breadcrumbs[] = [{ href: Routes.Members, label: "Members" }];
 
