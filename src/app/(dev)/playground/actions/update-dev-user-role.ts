7f921fd0bfd8554075b17229c66325d81c2f0e01
muhammad-bilal-ali-khokhar <o.engr.bilal@gmail.com>
2024-12-06 16:37:06 +0500

    fix: validate on cookies

--- src/app/(dev)/playground/actions/update-dev-user-role.ts
+++ src/app/(dev)/playground/actions/update-dev-user-role.ts
@@ -5,10 +5,15 @@ import { UpdateDevUserRoleType } from "../components/UpdateDevUserForm";
 import { intlNumberFormat } from "@/utils/formattedPhoneNumber";
 import { addCookies } from "@/utils/cookiesManager";
 import { Cookies } from "@/constant/constant";
+import { getLoggedInUser } from "@/utils/getLoggedInUser";
 
 export const updateDevUserRole = async (values: UpdateDevUserRoleType) => {
   const supabase = await getSupabaseClient();
   const formattedPhoneNumber = intlNumberFormat(values.phoneNumber);
+
+  const loggedInUser = await getLoggedInUser();
+
+  console.log({ loggedInUser,formattedPhoneNumber });
 
   const { data: updatedDevUser, error } = await supabase
     .from(Tables.Members)
@@ -23,10 +28,12 @@ export const updateDevUserRole = async (values: UpdateDevUserRoleType) => {
   if (error) {
     throw error;
   } else {
-    addCookies({
-      name: Cookies.LoginUser,
-      values: updatedDevUser,
-    });
-    return updatedDevUser;
+    if (loggedInUser.phoneNumber === formattedPhoneNumber) {
+      addCookies({
+        name: Cookies.LoginUser,
+        values: updatedDevUser,
+      });
+      return updatedDevUser;
+    }
   }
 };
