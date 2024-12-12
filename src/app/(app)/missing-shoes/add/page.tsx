a38044bc5f9a8e3e4e8d7a2d278c4cdd6b3dc386
nadirhussain786 <nh262464@gmail.com>
2024-12-12 18:14:10 +0500

    work on build fixes

--- src/app/(app)/missing-shoes/add/page.tsx
+++ src/app/(app)/missing-shoes/add/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MissingShoesFormBuilder } from '../components/MissingShoesFormBuilder';
+import { MissingShoesFormBuilder } from '../Components/MissingShoesFormBuilder';
 
 const Page = () => {
   return (
