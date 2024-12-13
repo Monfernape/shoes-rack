7581be1a141f2984b19a211df842d7483d81890f
nadirhussain786 <nh262464@gmail.com>
2024-12-13 18:30:33 +0500

    working on missing shoe details server action

---
+++ src/app/(app)/missing-shoes/actions/get-missing-shoe-by-id.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { Tables } from "@/lib/db";
+import { getSupabaseClient } from "@/utils/supabase/supabaseClient";
+
+export const getMissingShoeById = async (requestId: number) => {
+  const supabase = await getSupabaseClient();
+  const { data: missingShoe, error } = await supabase
+    .from(Tables.MissingShoes)
+    .select()
+    .eq("id", requestId)
+    .maybeSingle();
+
+  if (error) {
+    throw error.message;
+  }
+
+  return {
+    id: missingShoe.id,
+    status: missingShoe.status,
+    ownerName: missingShoe.owner_name,
+    time: new Date(missingShoe.time).toLocaleString(),
+    ownerPhoneNumber: missingShoe.owner_phone_number,
+    ownerAddress: missingShoe.owner_address,
+    description: missingShoe.description,
+    shoesToken: missingShoe.shoes_token,
+  };
+};
