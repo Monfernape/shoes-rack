e70d141cce407e5b1336d5e105694bf9cb3b99e2
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2024-12-20 00:41:34 +0500

    Removed

--- src/app/(app)/missing-shoes/components/MissingShoesHeader.tsx
+++ src/app/(app)/missing-shoes/components/MissingShoesHeader.tsx
@@ -1,13 +1,10 @@
 "use client";
-import React, { ChangeEvent, useEffect, useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+import { usePathname } from "next/navigation";
 import { Routes } from "@/lib/routes";
 import NavigationButton from "@/common/NavigationButton";
 import { Breadcrumbs } from "@/types";
 import { HeaderWrapper } from "@/common/HeaderWapper/HeaderWrapper";
-import { Input } from "@/components/ui/input";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
-import useDebounce from "@/hooks/useDebounce";
 
 interface Props {
   breadcrumbs: Breadcrumbs[];
@@ -15,41 +12,10 @@ interface Props {
 
 export const MissingShoesHeader = ({ breadcrumbs }: Props) => {
   const pathname = usePathname();
-  const router = useRouter();
-  const [search, setSearch] = useState("");
-  const debounceValue = useDebounce(search, 500);
-  const [isTitleHide, setIsTitleHide] = useState(false);
-
-  const handleSearchQueryChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-
-  useEffect(() => {
-    if (debounceValue) {
-      router.push(`${Routes.MissingShoes}?key=${debounceValue}`);
-    } else {
-      router.push(pathname);
-    }
-  }, [debounceValue, pathname, router]);
-
   return (
     <HeaderWrapper breadcrumbs={breadcrumbs}>
       {pathname === Routes.MissingShoes && (
         <div className="flex items-center space-x-2">
-          <div className="relative">
-            <Input
-              type="search"
-              placeholder="Search..."
-              data-testid="searchInput"
-              onFocus={() => setIsTitleHide(true)}
-              onBlur={() => setIsTitleHide(false)}
-              onChange={handleSearchQueryChange}
-              className={`pr-4 py-2 h-7 ${
-                isTitleHide ? "w-32 pl-10" : "w-2 pl-6"
-              } md:w-60 md:pl-10 rounded text-xs`}
-            />
-            <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600 w-4 h-4" />
-          </div>
           <NavigationButton
             path={Routes.AddMissingShoes}
             buttonText="Add Shoes"
