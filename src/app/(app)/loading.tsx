832da5be0c31ddd8976b88dc745b3ca9418c5849
Mubashar Shakeel <moonmubashar945@gmail.com>
2024-12-20 17:33:11 +0500

    fix: imported loader

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -1,10 +1,9 @@
+import { DataSpinner } from '@/common/Loader/Loader'
 import React from 'react'
 
 const loading = () => {
   return (
-    <div className="w-full h-full bg-loader fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-    <div className="w-16 h-16 border-4 border-gray-200 border-t-gray-800  border-solid rounded-full animate-spin"></div>
-  </div>
+    <DataSpinner/>
   )
 }
 
