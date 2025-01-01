0ba331516ebc97fd4c7c3f986cc50867bdfb4f54
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2025-01-01 20:00:02 +0500

    no need to hide

--- src/app/(app)/attendance/page.tsx
+++ src/app/(app)/attendance/page.tsx
@@ -19,9 +19,9 @@ const Page = async ({ searchParams }: { searchParams: { id: string } }) => {
   return (
     <>
       <AttendanceHeader breadcrumbs={breadcrumbs} />
-      {attendance?.length > 0 && (
-        <MemberFilter loginUser={loginUser} route={Routes.Attendance} />
-      )}
+
+      <MemberFilter loginUser={loginUser} route={Routes.Attendance} />
+
       <PageLayout>
         <AttendanceList attendance={attendance} loginUser={loginUser} />
       </PageLayout>
