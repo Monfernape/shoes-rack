c7ef1c0298e58b1f049c85fc775836266c28642b
muhammad-bilal-ali-khokhar <o.engr.bilal@gmail.com>
2025-01-02 15:46:07 +0500

    fix: refector code

--- src/app/(auth)/login/actions/loginUser.ts
+++ src/app/(auth)/login/actions/loginUser.ts
@@ -13,37 +13,48 @@ type LoginUser = {
 };
 
 export const loginUser = async ({ phoneNumber, password }: LoginUser) => {
+  const supabase = await getSupabaseClient();
+  const formattedPhoneNumber = intlNumberFormat(phoneNumber);
 
-    const supabase = await getSupabaseClient();
-    const formattedPhoneNumber = intlNumberFormat(phoneNumber);
-    try {
-    const { data: authUserData, error } =
-      await supabase.auth.signInWithPassword({
-        phone: formattedPhoneNumber,
-        password: password,
-      });
+  try {
+    const { data: authUserData } = await supabase.auth.signInWithPassword({
+      phone: formattedPhoneNumber,
+      password: password,
+    });
+    // Getting session
+    const { session } = authUserData;
 
-    if (error) {
+    addCookies({
+      name: Cookies.Session,
+      values: session,
+    });
+
+    redirect(Routes.Members);
+  } catch (error) {
+    if (error instanceof Error) {
       throw new Error(error.message);
     }
+  }
 
-    const { data: loginUser, error: userError } = await supabase
+  try {
+    const { data: loginUser } = await supabase
       .from(Tables.Members)
       .select("status")
       .eq("phoneNumber", formattedPhoneNumber)
       .single();
-
-    if (userError) {
-      throw new Error(userError.message);
-    }
-
     // Add a check to prevent deactivated users from logging in
-    if (loginUser.status === UserStatus.Deactivated) {
+    if (loginUser?.status === UserStatus.Deactivated) {
       throw new Error("User not exist");
     }
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+  }
 
+  try {
     // In success login case, update status and remove the invite link
-    const { data: updatedUser, error: updateError } = await supabase
+    const { data: updatedUser } = await supabase
       .from(Tables.Members)
       .update({
         status: UserStatus.Active,
@@ -52,26 +63,11 @@ export const loginUser = async ({ phoneNumber, password }: LoginUser) => {
       .eq("phoneNumber", formattedPhoneNumber)
       .select("*")
       .single();
-
-    if (updateError) {
-      throw new Error(updateError.message);
-    }
-
-    // Getting session
-    const { session } = authUserData;
-
     // addCookies function get name and values that will store against name
     addCookies({
       name: Cookies.LoginUser,
       values: updatedUser,
     });
-
-    addCookies({
-      name: Cookies.Session,
-      values: session,
-    });
-
-    redirect(Routes.Members);
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(error.message);
