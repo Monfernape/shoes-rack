f2a74f02bd16427d22403d1e7fdf518fa5dc79f7
muhammad-bilal-ali-khokhar <o.engr.bilal@gmail.com>
2025-01-02 15:30:11 +0500

    fix: update error

--- src/app/(auth)/login/actions/loginUser.ts
+++ src/app/(auth)/login/actions/loginUser.ts
@@ -17,60 +17,64 @@ export const loginUser = async ({ phoneNumber, password }: LoginUser) => {
     const supabase = await getSupabaseClient();
     const formattedPhoneNumber = intlNumberFormat(phoneNumber);
 
-    const { data: authUserData, error } = await supabase.auth.signInWithPassword({
-      phone: formattedPhoneNumber,
-      password: password,
-    });
+    const { data: authUserData, error } =
+      await supabase.auth.signInWithPassword({
+        phone: formattedPhoneNumber,
+        password: password,
+      });
 
     if (error) {
-      throw error;
-    } else {
-      const { data: loginUser, error } = await supabase
-        .from(Tables.Members)
-        .select("status")
-        .eq("phoneNumber", formattedPhoneNumber)
-        .single();
-
-      if (error) {
-        throw error;
-      } else {
-        // Add a check to prevent deactivated users from logging in
-        if (loginUser.status === UserStatus.Deactivated) {
-          throw new Error("User not exist");
-        } else {
-          // in success login case, update status and remove the invited link
-          const { data: loginUser, error } = await supabase
-            .from(Tables.Members)
-            .update({
-              status: UserStatus.Active,
-              invite_link: "",
-            })
-            .eq("phoneNumber", formattedPhoneNumber)
-            .select("*")
-            .single();
-          if (error) {
-            throw error;
-          } else {
-            // getting session
-            const { session } = authUserData;
-
-            // addCookies function get name and values that will store against name
-            addCookies({
-              name: Cookies.LoginUser,
-              values: loginUser,
-            });
-
-            addCookies({
-              name: Cookies.Session,
-              values: session,
-            });
-
-            redirect(Routes.Members);
-          }
-        }
-      }
+      throw new Error(error.message);
     }
+
+    const { data: loginUser, error: userError } = await supabase
+      .from(Tables.Members)
+      .select("status")
+      .eq("phoneNumber", formattedPhoneNumber)
+      .single();
+
+    if (userError) {
+      throw new Error(userError.message);
+    }
+
+    // Add a check to prevent deactivated users from logging in
+    if (loginUser.status === UserStatus.Deactivated) {
+      throw new Error("User not exist");
+    }
+
+    // In success login case, update status and remove the invite link
+    const { data: updatedUser, error: updateError } = await supabase
+      .from(Tables.Members)
+      .update({
+        status: UserStatus.Active,
+        invite_link: "",
+      })
+      .eq("phoneNumber", formattedPhoneNumber)
+      .select("*")
+      .single();
+
+    if (updateError) {
+      throw new Error(updateError.message);
+    }
+
+    // Getting session
+    const { session } = authUserData;
+
+    // addCookies function get name and values that will store against name
+    addCookies({
+      name: Cookies.LoginUser,
+      values: updatedUser,
+    });
+
+    addCookies({
+      name: Cookies.Session,
+      values: session,
+    });
+
+    redirect(Routes.Members);
   } catch (error) {
-    throw error;
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
   }
 };
