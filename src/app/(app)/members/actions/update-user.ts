f6c23286eccc14a3ba5a3737cef427c88b22063e
muhammad-bilal-ali-khokhar <o.engr.bilal@gmail.com>
2025-01-03 18:33:00 +0500

    fix: member cant edit also his details

--- src/app/(app)/members/actions/update-user.ts
+++ src/app/(app)/members/actions/update-user.ts
@@ -5,13 +5,17 @@ import { Tables } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { Routes } from "@/lib/routes";
 import { getEditPermissions } from "@/common/Actions/getEditPermissions";
+import { getLoggedInUser } from "@/utils/getLoggedInUser";
 
 export const updateUser = async (values: UpdateUser) => {
   const supabase = await getSupabaseClient();
 
+  const loggedInUser = await getLoggedInUser();
+  const isLoggedInUserMember = loggedInUser.member;
+
   const { hasEditPermission } = await getEditPermissions(values.id);
   
-  if (hasEditPermission) {
+  if (hasEditPermission || isLoggedInUserMember) {
     const { error } = await supabase
       .from(Tables.Members)
       .update({ ...values })
