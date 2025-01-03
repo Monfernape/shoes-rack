79305387d28bff3a9b6ceba0e18e3bd88093bfb7
nadirhussain786 <nh262464@gmail.com>
2025-01-03 17:33:11 +0500

    Build fixes

--- src/common/MemberSelector/MemberSelector.tsx
+++ src/common/MemberSelector/MemberSelector.tsx
@@ -50,7 +50,6 @@ const MemberSelector = ({ value, onValueChange , loginUser }: SelectFieldProps) 
     }
   }
   }, [members]);
-  console.log({ members })
   return (
     <Select
       value={value}
