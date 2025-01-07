38a96d726672ffaacf00925f2d70309854f54044
nadirhussain786 <nh262464@gmail.com>
2025-01-07 20:47:33 +0500

    work on typos

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { getLoggedInUser } from '@/utils/getLoggedInUser'
-import { Dashboard } from './components/Dashbord';
+import { Dashboard } from './components/Dashboard';
 
 const Page = async() => {
   const loggedUser = await getLoggedInUser();
