3122981173b1dad3bd37d44892e9f99215bf09e2
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2025-01-10 17:00:17 +0500

    Fixed bugs

--- src/app/layout/components/Sidebar.tsx
+++ src/app/layout/components/Sidebar.tsx
@@ -107,8 +107,15 @@ export const Sidebar = ({ isSidebarOpen, toggleSidebar }: Props) => {
   };
 
   return (
-    <aside
-      className={`
+    <>
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-gray-800 bg-opacity-50 z-20 lg:hidden"
+          onClick={toggleSidebar}
+        ></div>
+      )}
+      <aside
+        className={`
             ${isSidebarOpen || !isSmallScreen ? "block" : "hidden"}
             ${isSidebarOpen ? "block" : "hidden"}
              
@@ -118,56 +125,59 @@ export const Sidebar = ({ isSidebarOpen, toggleSidebar }: Props) => {
                 ${
                   isSidebarOpen ? "translate-x-0" : "-translate-x-full"
                 }  lg:translate-x-0 lg:block lg:fixed`}
-    >
-      <div className="flex flex-col h-full bg-sidebar-background">
-        <div className="flex items-center justify-between p-4 text-gray-700 hover:bg-gray-100 rounded-lg">
-          <h1 className={`text-sm font-medium text-gray-800 truncate sm:block`}>
-            Shoes Rack
-          </h1>
-          <Button
-            variant="ghost"
-            size="icon"
-            onClick={toggleSidebar}
-            className="lg:hidden"
-          >
-            <Cross1Icon className="h-4 w-4 text-gray-800" />
-          </Button>
-        </div>
-        <nav className="flex-1 overflow-y-auto">
-          <ul className="p-2 space-y-2 text-xs">
-            {routes.map((route, index) => (
-              <li key={`route-${index}`}>
-                <Link
-                  href={route.route}
-                  className={`flex items-center p-2 text-gray-700 ${
-                    route.route === pathname ? "bg-sidebar-active" : ""
-                  } hover:bg-gray-100 rounded-lg`}
-                >
-                  <span className="text-xs mr-3">
-                    {React.cloneElement(route.icon, {
-                      className: "w-3.5 h-3.5",
-                    })}
-                  </span>
-                  <span className="text-xs">{route.name}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="p-4 border-t">
-          <button
-            className="flex items-center w-full p-2 text-gray-700 hover:bg-gray-100 rounded-lg"
-            onClick={onLogoutUser}
-          >
-            <ExitIcon className="w-3.5 h-3.5 mr-3" />
-            <span className="text-xs">Logout</span>
-          </button>
-          {/* <button className="flex items-center w-full p-2 text-gray-700 hover:bg-gray-100 rounded-lg">
+      >
+        <div className="flex flex-col h-full bg-sidebar-background">
+          <div className="flex items-center justify-between p-4 h-[60px] text-gray-700 hover:bg-gray-100 rounded-lg">
+            <h1
+              className={`text-sm font-medium text-gray-800 truncate sm:block`}
+            >
+              Shoes Rack
+            </h1>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={toggleSidebar}
+              className="lg:hidden"
+            >
+              <Cross1Icon className="h-4 w-4 text-gray-800" />
+            </Button>
+          </div>
+          <nav className="flex-1 overflow-y-auto">
+            <ul className="p-2 space-y-2 text-xs">
+              {routes.map((route, index) => (
+                <li key={`route-${index}`}>
+                  <Link
+                    href={route.route}
+                    className={`flex items-center p-2 text-gray-700 ${
+                      route.route === pathname ? "bg-sidebar-active" : ""
+                    } hover:bg-gray-100 rounded-lg`}
+                  >
+                    <span className="text-xs mr-3">
+                      {React.cloneElement(route.icon, {
+                        className: "w-3.5 h-3.5",
+                      })}
+                    </span>
+                    <span className="text-xs">{route.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="p-2 border-t">
+            <button
+              className="flex items-center w-full p-2 text-gray-700 hover:bg-gray-100 rounded-lg"
+              onClick={onLogoutUser}
+            >
+              <ExitIcon className="w-3.5 h-3.5 mr-3" />
+              <span className="text-xs">Logout</span>
+            </button>
+            {/* <button className="flex items-center w-full p-2 text-gray-700 hover:bg-gray-100 rounded-lg">
             <GearIcon className="w-3.5 h-3.5 mr-3" />
             <span className="text-xs">Settings</span>
           </button> */}
+          </div>
         </div>
-      </div>
-    </aside>
+      </aside>
+    </>
   );
 };
