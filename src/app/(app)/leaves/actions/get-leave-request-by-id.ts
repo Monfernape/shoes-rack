1267475a228e3dc155de9bc5592eeb1d1dbda68a
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-01-27 19:27:21 +0500

    fix: on Loading  Removed Standard Page from background of the Attendance Route and update permission for leaves

--- src/app/(app)/leaves/actions/get-leave-request-by-id.ts
+++ src/app/(app)/leaves/actions/get-leave-request-by-id.ts
@@ -1,24 +1,50 @@
 "use server";
 
-import { AttendanceStatus } from "@/constant/constant";
+import { LeaveRequestStatus, MemberRole } from "@/constant/constant";
 import { Tables } from "@/lib/db";
 import { Routes } from "@/lib/routes";
+import { getLoggedInUser } from "@/utils/getLoggedInUser";
 import { getSupabaseClient } from "@/utils/supabase/supabaseClient";
 import { permanentRedirect } from "next/navigation";
 
 export const getLeaveRequestById = async (requestId: number) => {
   const supabase = await getSupabaseClient();
-  const { data, error } = await supabase
-    .from(Tables.Leaves)
-    .select()
-    .eq("id", requestId)
-    .maybeSingle();
+  const loginUser = await getLoggedInUser();
 
-  if (error) {
-    throw error.message;
+  try {
+    const { data: leaveDetails, error } = await supabase
+      .from(Tables.Leaves)
+      .select(
+        `*,
+      member:memberId (
+        name,
+        shift,role
+      )
+    `
+      )
+      .eq("id", requestId)
+      .maybeSingle();
+
+    let isPermission = true;
+    switch (loginUser.role) {
+      case MemberRole.Member:
+        isPermission = leaveDetails.member !== loginUser.id ? false : true;
+        break;
+      case MemberRole.ShiftIncharge:
+        if (leaveDetails.member.shift === loginUser.shift) {
+          isPermission =
+            leaveDetails.member.role === MemberRole.Member ? true : false;
+        } else {
+          isPermission = false;
+        }
+        break;
+    }
+    if (!isPermission || leaveDetails.status !== LeaveRequestStatus.Pending) {
+      return permanentRedirect(Routes.LeaveRequest);
+    }
+
+    return leaveDetails;
+  } catch (error) {
+    throw error;
   }
-  if (data.status === AttendanceStatus.Approve || data.status === AttendanceStatus.Reject ) {
-    permanentRedirect(Routes.LeaveRequest); 
-  }
-  return data;
 };
