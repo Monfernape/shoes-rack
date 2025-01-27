f83d0f089d589ed0b8206187b2518647eabf914f
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2025-01-27 15:22:42 +0500

    Fixed accessing through url  issues

--- src/app/(app)/leaves/actions/get-leave-request-by-id.ts
+++ src/app/(app)/leaves/actions/get-leave-request-by-id.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import { AttendanceStatus } from "@/constant/constant";
 import { Tables } from "@/lib/db";
+import { Routes } from "@/lib/routes";
 import { getSupabaseClient } from "@/utils/supabase/supabaseClient";
+import { permanentRedirect } from "next/navigation";
 
 export const getLeaveRequestById = async (requestId: number) => {
   const supabase = await getSupabaseClient();
@@ -14,6 +17,8 @@ export const getLeaveRequestById = async (requestId: number) => {
   if (error) {
     throw error.message;
   }
-
+  if (data.status === AttendanceStatus.Approve ) {
+    permanentRedirect(Routes.LeaveRequest); 
+  }
   return data;
 };
