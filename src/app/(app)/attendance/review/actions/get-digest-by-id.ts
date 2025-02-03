c0b6f4a682879b549e970f775e4656abd9d2382e
muhammad-bilal-ali-khokhar <o.engr.bilal@gmail.com>
2025-02-03 18:45:58 +0500

    fix: all sanario fixed

--- src/app/(app)/attendance/review/actions/get-digest-by-id.ts
+++ src/app/(app)/attendance/review/actions/get-digest-by-id.ts
@@ -17,47 +17,47 @@ export type Digest = {
   leaves: number[];
 };
 
-export const getDigestById = async (id: number) => {
+export const getDigestById = async () => {
   const supabase = await getSupabaseClient();
   const loggedInUser = await getLoggedInUser();
   const shift =
     loggedInUser.role === MemberRole.ShiftIncharge ? loggedInUser.shift : null;
 
-   
-    let digestData, digestError;
+  let digestData, digestError;
 
-    try {
-      const query = supabase.from(Tables.Digest).select("*").eq('id', id);
-      if (shift) {
-        query.eq('shift', shift);
-      }
-    
-      const { data, error } = await query;
-    
-      if (error || !data) {
-        throw new Error(error?.message || "No digest data found");
-      }
-    
-      // Process the data
-      digestData = {
-        id: data?.[0]?.id,
-        created_at: data?.[0]?.created_at,
-        status: data?.[0]?.status,
-        presents: data?.map(i => i.presents).flat(),
-        absents: data?.map(i => i.absents).flat(),
-        leaves: data?.map(i => i.leaves).flat(),
-      };
+  try {
+    const query = supabase.from(Tables.Digest).select("*");
 
+    if (shift) {
+      query.eq("shift", shift).single();
+    }
+
+    const { data, error } = await query;
+
+    if (error || !data) {
+      throw new Error(error?.message || "No digest data found");
+    }
+
+    // Process the data
+    digestData = {
+      id: data?.[0]?.id,
+      created_at: data?.[0]?.created_at,
+      status: data?.[0]?.status,
+      presents: data?.map((i) => i.presents).flat(),
+      absents: data?.map((i) => i.absents).flat(),
+      leaves: data?.map((i) => i.leaves).flat(),
+    };
 
     if (digestError || !digestData) {
       console.error(
         "Error fetching digest:",
-        digestError?.message || "No digest data found"
+        digestError || "No digest data found"
       );
     }
 
+    console.log({ digestData });
 
-    if(digestData && !Array.isArray(digestData)){
+    if (digestData && !Array.isArray(digestData)) {
       // Fetch the attendance and leaves data concurrently
       if (digestData.status === DigestStatus.Confirmed) {
         const [
@@ -103,7 +103,7 @@ export const getDigestById = async (id: number) => {
               ).toUTCString()
             ),
         ]);
-  
+
         if (
           attendanceResponsePresents.error ||
           attendanceResponseAbsents?.error ||
@@ -111,16 +111,12 @@ export const getDigestById = async (id: number) => {
         ) {
           throw new Error();
         }
-  
-        const attendanceListPresents = attendanceResponsePresents.data || [];
-        const attendanceListAbsents = attendanceResponseAbsents.data || [];
-        const leavesList = leavesResponse.data || [];
-  
-        // Process absents and presents based on the attendance status
-        const absents = attendanceListAbsents;
-        const presents = attendanceListPresents;
-        // Assuming leaves are represented by the `leavesList`
-        const leaves = leavesList;
+
+        const presents = attendanceResponsePresents.data || [];
+        const absents = attendanceResponseAbsents.data || [];
+        const leaves = leavesResponse.data || [];
+
+        console.log({ presents, absents, leaves });
         return {
           id: digestData.id,
           created_at: digestData.created_at,
@@ -133,90 +129,101 @@ export const getDigestById = async (id: number) => {
         // Fetch members
         const { data: members, error: membersError } = await supabase
           .from(Tables.Members)
-          .select("id")
-          .eq('status', UserStatus.Active);
-          
-  
+          .select("id, name, shift")
+          .eq("status", UserStatus.Active);
+
         if (membersError) {
           console.error("Error fetching members:", membersError?.message);
           return null;
         }
-  
+
         // Fetch attendance and leaves for the current date
         const [attendancesResponse, leavesResponse] = await Promise.all([
-          supabase
-            .from(Tables.Attendance)
-            .select("*, members(name, shift)")
-            .filter(
-              "created_at",
-              "gte",
-              new Date(Date.now() - 864e5).toUTCString()
-            ),
-          supabase
-            .from(Tables.Leaves)
-            .select("*, members(name, shift)")
-            .filter(
-              "created_at",
-              "gte",
-              new Date(Date.now() - 864e5).toUTCString()
-            ),
+          supabase.from(Tables.Attendance).select("*, members(name, shift)"),
+          // .filter(
+          //   "created_at",
+          //   "gte",
+          //   new Date(Date.now() - 864e5).toUTCString()
+          // ),
+          supabase.from(Tables.Leaves).select("*, members(name, shift)"),
+          // .filter(
+          //   "created_at",
+          //   "gte",
+          //   new Date(Date.now() - 864e5).toUTCString()
+          // ),
         ]);
-  
+
         if (attendancesResponse.error) {
-          console.error("Error fetching attendances:", attendancesResponse.error);
+          console.error(
+            "Error fetching attendances:",
+            attendancesResponse.error
+          );
           return null;
         }
-  
+
         if (leavesResponse.error) {
           console.error("Error fetching leaves:", leavesResponse.error);
           return null;
         }
-  
+
         const attendances = attendancesResponse.data || [];
         const leavesList = leavesResponse.data || [];
-  
+
         // get members ids that are exists in leaves and attendances
         const attendanceMemberIds = attendances
           .map((a) => a.memberId)
           .filter(Boolean);
-        const leaveMemberIds = leavesList.map((l) => l.memberId).filter(Boolean);
-  
+
+        const leaveMemberIds = leavesList
+          .map((l) => l.memberId)
+          .filter(Boolean);
+
         // Members who have no attendance or leave for the current date
-        const membersWithNoRecords = members.filter(
-          (member) =>
+        const membersWithNoRecords = members.filter((member) => {
+          if (
             !attendanceMemberIds.includes(member.id) &&
             !leaveMemberIds.includes(member.id)
-        );
-  
+          ) {
+            return member;
+          }
+        });
+
+        const presentsMembers = attendances
+          .map((attendance) => {
+            if (
+              attendance.status === AttendanceStatus.Approve ||
+              attendance.status === AttendanceStatus.Pending
+            ) {
+              return attendance;
+            }
+          })
+          .filter(Boolean);
+
+        const absentsMembers = attendances
+          .map((attendance) => {
+            if (attendance.status === AttendanceStatus.Reject) {
+              return attendance;
+            }
+          })
+          .filter(Boolean);
+
         // Insert rejected attendance for these members
-        // const newAttendances = membersWithNoRecords.map((member) => ({
-        //   memberId: member.id,
-        //   status: AttendanceStatus.Reject,
-        //   startTime: "00:00",
-        //   endTime: "00:00",
-        //   created_at: `${new Date(Date.now() - 864e5).toUTCString()}`,
-        // }));
-        // if (newAttendances.length > 0) {
-        //   const { error: insertError } = await supabase
-        //     .from(Tables.Attendance)
-        //     .insert(newAttendances);
-        //   if (insertError) {
-        //     throw insertError;
-        //   }
-        // }
-  
-        const { data: attendancesList, error: attendanceError } = await supabase
-          .from(Tables.Attendance)
-          .select("*, members(name, shift)");
-  
-        if (attendanceError) {
-          return {
-            error: attendanceError.message,
-          };
-        }
-  
-        const absentsMembersIds = new Set(
-          attendancesList
+        const absentsMembersLis = membersWithNoRecords.map((member) => ({
+          memberId: member.id,
+          status: AttendanceStatus.Reject,
+          startTime: "00:00",
+          endTime: "00:00",
+          created_at: `${new Date(Date.now() - 864e5).toUTCString()}`,
+          members: {
+            name: member.name,
+            shift: member.shift,
+          },
+        }));
+
+        const allAbsentsMembers = [...absentsMembers, ...absentsMembersLis];
+
+        const absentsItems = new Set(
+          allAbsentsMembers
             .map((a) => {
               if (a.status === AttendanceStatus.Reject) {
                 return a;
@@ -224,37 +231,32 @@ export const getDigestById = async (id: number) => {
             })
             .filter(Boolean)
         );
-  
-        const presentsMembersIds = new Set(
-          attendancesList
+
+        const presentsItems = new Set(
+          presentsMembers
             .map((a) => {
-              if (a.status === AttendanceStatus.Approve) {
+              if (
+                a.status === AttendanceStatus.Approve ||
+                a.status === AttendanceStatus.Pending
+              ) {
                 return a;
               }
             })
             .filter(Boolean)
         );
-  
-        const leavesMembersIds = new Set(
+
+        const leavesItems = new Set(
           leavesList
             .map((a) => {
               return a;
             })
             .filter(Boolean)
         );
-  
-        const absents = Array.from(absentsMembersIds);
-        const presents = Array.from(presentsMembersIds);
-        const leaves = Array.from(leavesMembersIds);
-  
-        console.log({
-          id: digestData.id,
-          created_at: digestData.created_at,
-          status: digestData.status,
-          presents,
-          absents,
-          leaves,
-        });
+
+        const absents = Array.from(absentsItems);
+        const presents = Array.from(presentsItems);
+        const leaves = Array.from(leavesItems);
+
         return {
           id: digestData.id,
           created_at: digestData.created_at,
