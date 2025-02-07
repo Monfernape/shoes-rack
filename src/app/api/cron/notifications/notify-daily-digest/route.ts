8680b0264a04e4e3fce5b98918004318c4d67407
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-02-07 17:01:03 +0500

    fix:add notification insertion in cron job

--- src/app/api/cron/notifications/notify-daily-digest/route.ts
+++ src/app/api/cron/notifications/notify-daily-digest/route.ts
@@ -1,7 +1,13 @@
 import { Tables } from "@/lib/db";
 import { getSupabaseClient } from "@/utils/supabase/supabaseClient";
 import { NextResponse } from "next/server";
-import { DigestStatus, Shift } from "@/constant/constant";
+import {
+  DigestStatus,
+  MemberRole,
+  NotificationType,
+  Shift,
+  UserStatus,
+} from "@/constant/constant";
 
 export async function POST() {
   try {
@@ -12,15 +18,49 @@ export async function POST() {
       presents: [],
       leaves: [],
     };
-    const { error } = await supabase.from(Tables.Digest).insert([
+    // Insert All digest into Digest Table
+    const { error: digestError } = await supabase.from(Tables.Digest).insert([
       { ...digest, shift: Shift.ShiftA },
       { ...digest, shift: Shift.ShiftB },
       { ...digest, shift: Shift.ShiftC },
       { ...digest, shift: Shift.ShiftD },
     ]);
 
-    if (error) {
-      throw error;
+    if (digestError) {
+      return digestError;
+    }
+    // Get All Active Shift Incharges
+    const { data: shiftIncharges, error: memberError } = await supabase
+      .from(Tables.Members)
+      .select(`id,shift,role`)
+      .eq("status", UserStatus.Active)
+      .eq("role", MemberRole.ShiftIncharge);
+
+    if (memberError) {
+      return memberError;
+    }
+
+    const attendanceNotifcations = {
+      title: "Review Attendance",
+      is_read: false,
+      system_generated: true,
+      description: "Please Review Attendances",
+      type: NotificationType.Digest,
+    };
+
+    const shiftNotifications = shiftIncharges?.map((shiftIncharge) => {
+      return {
+        ...attendanceNotifcations,
+        member_id: shiftIncharge.id,
+      };
+    });
+    // insert shift incharge notifications to Notification Table
+    const { error: notificationError } = await supabase
+      .from(Tables.Notification)
+      .insert(shiftNotifications);
+
+    if (notificationError) {
+      throw notificationError;
     }
     return NextResponse.json({ data: "Success", status: 200 });
   } catch (error) {
