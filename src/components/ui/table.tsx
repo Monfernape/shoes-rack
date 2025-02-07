95e254599c104df332ecfa632c7e1a8081b8ecda
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-02-07 19:38:37 +0500

    fix:removed scroll issue

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto sm:overflow-hidden">
+  <div className="relative w-full">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-xs text-table-trow", className)}
