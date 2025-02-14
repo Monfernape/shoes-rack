98f452502c8f263e9abb6fec5551eaad085b4df3
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-02-14 19:09:44 +0500

    fix:Fixed Notifications Layout issues

--- src/app/(app)/notification/page.tsx
+++ src/app/(app)/notification/page.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { BellIcon } from "lucide-react";
 import { StandardPage } from "@/common/StandardPage/StandardPage";
 import { HeaderWrapper } from "@/common/HeaderWapper/HeaderWrapper";
 import { Breadcrumbs } from "@/types";
 import { Routes } from "@/lib/routes";
+import { getNotificationsCount } from "./actions/get-notifications-count";
+import { Inbox } from "lucide-react";
 
 const breadcrumbs: Breadcrumbs[] = [
   { href: Routes.Notification, label: "Notification" },
 ];
 
 const Page = async () => {
+  const countNotifications = await getNotificationsCount();
   const standarPageProps = {
     title: "Notification",
-    description: `There are 169 unread Notifications `,
+    description: `There are ${countNotifications} unread Notifications `,
     actionButton: false,
-    buttonIcon: <BellIcon />,
+    icon: <Inbox />,
     hasContent: false,
   };
   return (
