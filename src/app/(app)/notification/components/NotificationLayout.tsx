c2c58c822831cc8e2638d22fd95c9b03ff75ea38
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-02-14 14:31:49 +0500

    feat(Notifications):Added Delete Notifications and Update Layout

--- src/app/(app)/notification/components/NotificationLayout.tsx
+++ src/app/(app)/notification/components/NotificationLayout.tsx
@@ -1,12 +1,15 @@
 "use client";
-import React from "react";
-import { Dot as DotIcon } from "lucide-react";
+import React, { useState } from "react";
+import { AlignJustify, Dot as DotIcon } from "lucide-react";
 import Link from "next/link";
 import { Routes } from "@/lib/routes";
 import { Notifications } from "@/types";
 import { formatDistance } from "date-fns";
 import { usePathname } from "next/navigation";
+
 import { updateNotificationStatus } from "../actions/update-notification-status";
+import { Sidebar } from "@/app/layout/components/Sidebar";
+import { Button } from "@/components/ui/button";
 type NotificationLayoutProps = {
   children: React.ReactNode;
   allnotification: Notifications[];
@@ -16,25 +19,43 @@ export default function NotificationLayout({
   allnotification,
 }: NotificationLayoutProps) {
   const pathname = usePathname();
+  const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
 
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
   const notificationId = pathname.split("/")[2];
   const handleNotificationStatus = async (id: number) => {
     await updateNotificationStatus(id);
   };
 
   return (
-    <div className="flex  h-full ">
+    <div className="flex  h-full">
       <div
-        className={`w-full  lg:w-96  border-r h-full ${
+        className={`w-full  lg:w-96  border-r h-full   ${
           notificationId && "hidden lg:block"
         } `}
       >
+        {isSidebarOpen && (
+          <Sidebar
+            isSidebarOpen={isSidebarOpen}
+            toggleSidebar={toggleSidebar}
+          />
+        )}
         <div className="flex   items-center h-16 py-4 px-5 border-b">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={toggleSidebar}
+            className="lg:hidden"
+          >
+            <AlignJustify className="h-4 w-4 text-gray-800" />
+          </Button>
           <h4 className="font-medium text-sm text-center flex items-center">
             Notifications
           </h4>
         </div>
-        <ul className="text-gray-700  ">
+        <ul className="text-gray-700 overflow-y-scroll h-full  ">
           {allnotification?.map((notification) => {
             return (
               <li className=" border-b " key={notification.id}>
@@ -50,7 +71,7 @@ export default function NotificationLayout({
                   href={`${Routes.Notification}/${notification.id}`}
                 >
                   <div className="flex   w-full gap-2">
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 flex-1">
                       <div className="flex justify-between items-center">
                         <p className="font-normal text-sm ">
                           {notification.title}
@@ -67,7 +88,7 @@ export default function NotificationLayout({
                         </p>
                       </div>
                     </div>
-                    <div className="flex align-middle items-center flex-col flex-1">
+                    <div className="flex align-middle items-center flex-col  mr-12 lg:mr-0">
                       <div className="min-w-8 h-8 ">
                         {!notification.is_read && (
                           <DotIcon className="w-8 h-auto mt-[-8px]" />
